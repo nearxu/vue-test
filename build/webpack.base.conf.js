bfba486c1fc1b55f1ba7967ee18909ec9254c5f1
nearxu <2448895924@qq.com>
2019-07-18 15:39:51 +0800

    add model component

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -52,6 +52,11 @@ module.exports = {
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
+        test: /\.scss$/,
+        loader: ["style", "css", "sass"],
+        include: [resolve('src')]
+      },
+      {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
--- src/components/foo.vue
+++ src/components/foo.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
     <h2>{{ msg }}</h2>
+    <button @click="toggle">toggle model</button>
+    <v-model :isShow="isShow" @onClose="onClose">
+      <div slot="header">插入到name为header的slot标签里面</div>
+      <div class="dialog_publish_main" slot="main">
+        <div>
+          <label for="name"></label>
+          <input type="text" name="name" v-model="name" />
+          <label for="password"></label>
+          <input type="text" name="password" v-model="password" />
+          <button @click="submit">submit</button>
+        </div>
+      </div>
+    </v-model>
     <router-link :to="'/foo1'">foo1</router-link>
     <router-link :to="'/foo2'">foo2</router-link>
     <router-view></router-view>
@@ -8,11 +21,16 @@
 </template>
 
 <script>
+import Model from './model'
+
 export default {
   name: 'Foo',
   data () {
     return {
-      msg: 'hello foo'
+      msg: 'hello foo',
+      isShow: false,
+      name: '',
+      password: ''
     }
   },
   computed: {
@@ -24,7 +42,19 @@ export default {
   methods: {
     goBack () {
       window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    },
+    onClose () {
+      this.isShow = false
+    },
+    toggle () {
+      this.isShow = true
+    },
+    submit (e) {
+      console.log(e)
     }
+  },
+  components: {
+    'v-model': Model
   }
 }
 </script>
